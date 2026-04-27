e2896c3011f5c79cd06041c016f1dec29740435e
NguyenHoang <vnhantao1@gmail.com>
2026-04-27 16:16:16 +0700

    sửa FileProcessor và workflows.yaml

--- src/main/java/App/FileProcessor.java
+++ src/main/java/App/FileProcessor.java
@@ -5,8 +5,6 @@ import java.nio.file.Paths;
 
 public class FileProcessor {
     public String getFilePath() {
-        // Sử dụng Paths.get để tự động thêm dấu phân cách đúng theo OS
-        Path path = Paths.get("data", "config.txt");
-        return path.toString();
+        return java.nio.file.Paths.get("data", "config.txt").toString();
     }
 }

4dab054ef14dd57f7e0065bf0f8fc006406d8fad
NguyenHoang <vnhantao1@gmail.com>
2026-04-27 16:11:59 +0700

    sửa file Test và file FileProcessor

--- src/test/java/App/AppTest.java
+++ src/test/java/App/AppTest.java
@@ -2,10 +2,8 @@ package App;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.File;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class AppTest {
     @Test
@@ -13,8 +11,6 @@ class AppTest {
         FileProcessor processor = new FileProcessor();
         String path = processor.getFilePath();
 
-        // Kiểm tra xem đường dẫn có chứa dấu phân cách chuẩn của hệ thống đang chạy không
-        // File.separator sẽ là \ trên Windows và / trên Linux
         assertTrue(path.contains(File.separator));
         System.out.println("Path running on this OS: " + path);
     }
